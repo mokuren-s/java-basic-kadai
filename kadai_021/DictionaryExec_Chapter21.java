00dcbf7df39e9713730df2cc3822b3b39f292dc7
yasuyuki-sugita <disk.bc.magnolia.25@gmail.com>
2024-12-22 18:00:35 +0900

    課題提出

---
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -0,0 +1,17 @@
+package kadai_021;
+
+public class DictionaryExec_Chapter21 {
+
+	public static void main(String[] args) {
+		// 辞書クラスをインスタンス化
+		Dictionary_Chapter21 dictionary = new Dictionary_Chapter21();
+		
+		// 調べる英単語を配列にセットする
+		String[] wordsArray = {"apple", "banana", "grape", "orange"};
+		
+		// 辞書を調べるメソッドを引数指定で呼び出す
+		dictionary.lookUpWords(wordsArray);
+			
+	}
+
+}
